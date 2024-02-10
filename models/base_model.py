2e4c9e14c8c2e5ba1af2ef5c9e3db50940550508
atharhasan <athar_h12@yahoo.com>
2024-02-10 10:46:42 +0200

    commit task

--- models/base_model.py
+++ models/base_model.py
@@ -7,14 +7,21 @@ from datetime import datetime
 
 class BaseModel:
     
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """BaseModel class that defines all 
         common attributes/methods for other classes
         
         """
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.fromisoformat(value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
         
     def __str__(self):
         return("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
@@ -28,4 +35,4 @@ class BaseModel:
         dict_set['__class__'] = self.__class__.__name__
         dict_set['created_at'] = self.created_at.isoformat()
         dict_set['updated_at'] = self.updated_at.isoformat()
-        
+        return dict_set
