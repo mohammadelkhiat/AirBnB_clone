6243fe05d62bbe793cd2a160423abe1c4ca1af04
atharhasan <athar_h12@yahoo.com>
2024-02-11 01:30:36 +0200

    commit task

--- models/base_model.py
+++ models/base_model.py
@@ -31,7 +31,7 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new()
+            models.storage.new(self)
         
     def __str__(self):
         return("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
