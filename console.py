e31e6a43f15c8ba1bac213f99d20e5c1e1377bf7
Mohammad <mohammadelkhiat@gmail.com>
2024-02-11 03:57:01 +0200

    0_0

--- console.py
+++ console.py
@@ -2,6 +2,7 @@
 """here is the console that user should interact with"""
 
 import cmd
+import models.engine
 
 
 class HBNBCommand(cmd.Cmd):
@@ -17,6 +18,16 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         pass
+
+    def do_create(self, arg):
+        if not arg:
+            print("** class name missing **")
+        elif arg not in self.classes:
+            print("** class doesn't exist **")
+        else:
+            new_instance = self.classes[arg]()
+            new_instance.save()
+            print(new_instance.id)
 
 
 if __name__ == '__main__':
