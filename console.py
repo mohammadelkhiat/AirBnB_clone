08ab487a16ea57470e839893731d22ecb3fb0a37
Mohammad <mohammadelkhiat@gmail.com>
2024-02-11 03:21:54 +0200

    0_0

--- console.py
+++ console.py
@@ -3,6 +3,20 @@
 
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
     """class defintion"""
-    
+    PROMPT = "(hbnb)"
+
+    def do_EOF(self, line):
+        "EXIT"
+        return True
+
+    def do_quit(self, line):
+        return True
+
+    def emptyline(self):
+        pass
+
+    if __name__ == '__main__':
+        HBNBCommand().cmdloop()
