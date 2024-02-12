18985e8434e879ae0a2ef23a69f0b7b466e63d8c
Mohammad <mohammadelkhiat@gmail.com>
2024-02-12 04:12:29 +0200

    0_0

--- models/user.py
+++ models/user.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """Building the user info"""
 
-import cmd
-import models.engine
 from models.base_model import BaseModel
-from models import storage
+
 
 class User(BaseModel):
     """Clerifaing"""
