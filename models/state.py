d96967016569ff953a9c44ac2c7fbdf16a2a0df9
atharhasan <athar_h12@yahoo.com>
2024-02-12 04:35:08 +0200

    commit task

--- models/state.py
+++ models/state.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 '''state model class'''
 
+
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
     """State class that inherit from BaseModel classe"""
+
     name = ""
