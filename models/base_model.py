95cf24637304243c38f0312b8f689ed7040c8de6
Mohammad <mohammadelkhiat@gmail.com>
2024-02-12 04:48:20 +0200

    0_0

--- models/base_model.py
+++ models/base_model.py
@@ -29,15 +29,15 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             models.storage.new(self)
-        
+
     def __str__(self):
-        return("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
-    
+        return ("[{}] ({}) {}".format
+                (self.__class__.__name__, self.id, self.__dict__))
+
     def save(self):
         self.updated_at = datetime.now()
         models.storage.save()
-        
-        
+
     def to_dict(self):
         dict_set = self.__dict__.copy()
         dict_set['__class__'] = self.__class__.__name__
